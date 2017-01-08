19763e932292fd5058e4fc1531174e6613b7083f
Sara Lindström <saralindstrom@Saras-MacBook-Pro.local>
2017-01-08 11:55:04 -0500

    Initial Commit

---
+++ spaceinvaders/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  spaceinvaders
+//
+//  Created by Sara Lindström on 2017-01-08.
+//  Copyright © 2017 Sara Lindström. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
